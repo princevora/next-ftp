38547f3117a36cf83672ffb97154674b3013c60f
Prince <princevora684@gmail.com>
2024-11-01 11:18:07 +0530

    update message text error

--- src/app/connect/page.jsx
+++ src/app/connect/page.jsx
@@ -1,30 +1,26 @@
-"use client";
+'use client';
 
+import PropTypes from 'prop-types';
 import { Button, Typography } from "@material-tailwind/react";
 import Link from "next/link";
-import { useEffect, useState } from "react";
 
-export default function ConnectionError({ messageText }) {
-    const [message, setMessage] = useState("Please use Ftp Details Form To Submit The data you can navigate through the button Below");
-
-    useEffect(() => {
-        if (messageText) {
-            setMessage(messageText);
-        }
-    }, [])
-
+export default function ConnectionError(props) {
     return (
         <div className="max-w-full mx-auto my-56">
             <Typography variant="paragraph" className="text-center">
-                {message}
+                Please use Ftp Details Form To Submit The data you can navigate through the button Below
             </Typography>
             <div className="flex justify-center py-5">
                 <Button variant="gradient" color="gray">
-                    <Link color="indigo" href="/">
+                    <Link href="/">
                         Go Home
                     </Link>
                 </Button>
             </div>
         </div>
-    )
+    );
 }
+
+ConnectionError.propTypes = {
+    messageText: PropTypes.string,
+};
