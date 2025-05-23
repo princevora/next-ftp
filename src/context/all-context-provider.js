aee97dbfb635508586e722594ddc9a9c4192fd90
princevora <princevora684@gmail.com>
2025-05-23 14:12:09 +0530

    update context providers

--- src/context/all-context-provider.js
+++ src/context/all-context-provider.js
@@ -1,47 +1,67 @@
-"use client"
+"use client";
 
+import React from "react";
+
+// Individual contexts
 import RenameContext from "./RenameItemContext";
 import RenameConfirmationContext from "./confirmation";
 import CreateItemContext from "./create-item";
+import MoveItemContext from "./move-item";
+import UploadContext from "./file-upload";
+import SidebarContext from "./sidebar";
+import BulkSelectContext from "./bulk-select";
+import ContextMenuContext from "./context-menu";
+import RefContext from "./ref";
 import FtpDetailContext from "./ftp-details-context";
 import PasswordContext from "./encrypt-password";
 import SearchPathContextProvider from "./search-path";
-import BulkSelectContext from "./bulk-select";
-import SidebarContext from "./sidebar";
-import UploadContext from "./file-upload";
-import ContextMenuContext from "./context-menu";
-import MoveItemContext from "./move-item";
-import RefContext from "./ref";
+
+// Compose helper
+const composeProviders = (...providers) =>
+  providers.reduce(
+    (Accumulated, Current) =>
+      ({ children }) =>
+        (
+          <Accumulated>
+            <Current>{children}</Current>
+          </Accumulated>
+        ),
+    ({ children }) => <>{children}</>
+  );
+
+// Grouped providers for scalability
+const FileSystemContexts = ({ children }) => (
+  <RenameContext>
+    <RenameConfirmationContext>
+      <CreateItemContext>
+        <MoveItemContext>
+          <UploadContext>{children}</UploadContext>
+        </MoveItemContext>
+      </CreateItemContext>
+    </RenameConfirmationContext>
+  </RenameContext>
+);
+
+const UIContexts = ({ children }) => (
+  <SidebarContext>
+    <BulkSelectContext>
+      <ContextMenuContext>{children}</ContextMenuContext>
+    </BulkSelectContext>
+  </SidebarContext>
+);
+
+// Final composition
+const AllContexts = composeProviders(
+  RefContext,
+  FtpDetailContext,
+  PasswordContext,
+  SearchPathContextProvider,
+  FileSystemContexts,
+  UIContexts
+);
 
 const UseGlobalContext = ({ children }) => {
-    return (
-        <RefContext>
-            <UploadContext>
-                <MoveItemContext>
-                    <SidebarContext>
-                        <RenameContext>
-                            <PasswordContext>
-                                <CreateItemContext>
-                                    <BulkSelectContext>
-                                        <FtpDetailContext>
-                                            <ContextMenuContext>
-                                                <RenameConfirmationContext>
-                                                    <SearchPathContextProvider>
-                                                        {children}
-                                                    </SearchPathContextProvider>
-                                                </RenameConfirmationContext>
-                                            </ContextMenuContext>
-                                        </FtpDetailContext>
-                                    </BulkSelectContext>
-                                </CreateItemContext>
-                            </PasswordContext>
-                        </RenameContext>
-                    </SidebarContext>
-                </MoveItemContext>
-            </UploadContext>
-        </RefContext>
-    );
+  return <AllContexts>{children}</AllContexts>;
 };
-
 
 export default UseGlobalContext;
